78a4627d552f5c0ec86b293579206e6187cc3c65
yobmod <yobmod@gemail.com>
2018-04-27 10:04:35 +0100

    add syntaxhighlight rules

--- rpi/webvid.py
+++ rpi/webvid.py
@@ -13,11 +13,13 @@ while(cap.isOpened()):
     if ret is True:
         # Our operations on the frame come here
         gray: np.ndarray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        flipped: np.ndarray = cv2.flip(frame, 0)  # 0=vert, 1=horiz, -1=both       # Display the resulting frame
+        vflipped: np.ndarray = cv2.flip(frame, 0)  # 0=vert, 1=horiz, -1=both       # Display the resulting frame
+        hflipped: np.ndarray = cv2.flip(frame, 1)
 
-        cv2.imshow('frame', frame)
-        cv2.imshow('frame1', gray)
-        cv2.imshow('frame2', flipped)
+        # cv2.imshow('frame', frame)
+        # cv2.imshow('frame1', gray)
+        # cv2.imshow('frame2', vflipped)
+        cv2.imshow('frame2', hflipped)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
